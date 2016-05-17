c6b7e41b55798fc83b153fb913b842c1518b484d
Ruslan Yanberdin <happierall@gmail.com>
2016-05-17 22:03:43 +0300

    [FIX] date in chat

--- src/project/chat/utils.js
+++ src/project/chat/utils.js
@@ -2,7 +2,7 @@ export const formatDatetime = unixtime => {
   let date = new Date(unixtime * 1000);
   let minutes = "0" + date.getMinutes();
 
-  return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${minutes.substr(-2)}`;
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${minutes.substr(-2)}`;
 };
 
 export const formatTime = unixtime => formatDatetime(unixtime).split(' ')[1];
