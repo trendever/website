93443dffe2e50962fd2a57170036d22f4e73ed36
Ruslan Yanberdin <happierall@gmail.com>
2016-05-23 13:50:01 +0300

    [FIX] mixpanel (shim for all)

--- src/mixpanel-browser.js
+++ src/mixpanel-browser.js
@@ -1,8 +1,8 @@
 /* globals window */
 
-if (window.location.protocol !== "https:") {
-  var mixpanel = require('mixpanel-browser/build/mixpanel.cjs');
-} else {
+// if (window.location.protocol !== "https:") {
+//   var mixpanel = require('mixpanel-browser/build/mixpanel.cjs');
+// } else {
   let _methods = "default.init init disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
 
   var mixpanel = {};
@@ -13,7 +13,7 @@ if (window.location.protocol !== "https:") {
     }
   }
   window.mixpanel = mixpanel;
-}
+// }
 
 // Shimmed mixpanel api if protocol != https.
 export default mixpanel
