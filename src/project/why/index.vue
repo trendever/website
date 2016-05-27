206ac7c21b93c3a1e3c18a2e88cd694e550312f5
dmgame <denis.m.pcspace@gmail.com>
2016-05-27 19:32:20 +0200

    Beta 27.05

--- src/project/why/index.vue
+++ src/project/why/index.vue
@@ -17,14 +17,14 @@
           img(src='img/logo.svg' alt='')
       a(href='#').btn-yellow.btn-yellow__s Войти
   .section.top
-    .section__content(v-el:main-video-cnt)
-      iframe(src='https://www.youtube.com/embed/iNJdPyoqt8U',
-            :width='mainVideoWidth', :height='mainVideoHeight',
-            frameborder='0', webkitallowfullscreen,
-            mozallowfullscreen, allowfullscreen)
 
     .arithmetic
-      img(src='img/arithmetic.svg' alt ='')
+      .arithmetic__first
+        img(src='img/instagramm.png' alt ='')
+      .arithmetic__svg
+        img(src='img/arithmetic.svg' alt ='')
+      .arithmetic__result
+        img(src='img/result.png' alt ='')
 
   .section.inform-item
     .section__content
@@ -32,16 +32,16 @@
         div.inform-item__title
           p Любишь искать и покупать
             br
-            | что-то новенькое
+            |  что-то новенькое
             br
-            | через Instagram?
+            |  через Instagram?
       .inform-item__answer
         div.inform-item__title.inform-item__answer__title
           p Вот только среди
             br
-            | множества товаров
+            |  множества товаров
             br
-            | без категорий, отзывов и цен...
+            |  без категорий, отзывов и цен...
           span ...шопинг в Instagram становится испытанием!
 
   .section.inform-item
@@ -64,7 +64,7 @@
         div.inform-item__title.inform-item__answer__title
           p Трендскауты,
             br
-            | наши fashion-эксперты,
+            |  наши fashion-эксперты,
           span ...уже все сделали для тебя!
 
   .section.inform-item
@@ -73,95 +73,163 @@
         div.inform-item__title
           ul.check-list
             li
-              i.ic-checked
+              i.ic-galochka
               Выбрали самое интересное
             li
-              i.ic-checked
+              i.ic-galochka
               Узнали все цены
             li Отсортировали по категориям
-              i.ic-checked
+              i.ic-galochka
             li
-              i.ic-checked
+              i.ic-galochka
               Проверили продавцов
           span Пара минут, и ты найдешь то, что нужно!
       .inform-item__answer
         div.inform-item__title.inform-item__answer__title
           p.with-padding Подписывайся
             br
-            | на ленты Трендскаутов,
+            |  на ленты Трендскаутов,
           span ...которые выбрали тренды не на продажу, а для себя.
             br
-            | Любой тренд можно сохранить в ленту своих желаний.
+            |  Любой тренд можно сохранить в ленту своих желаний.
 
   .section.inform-item
     .section__content
       .inform-item__top-block.inform-item__bg-yellow.gallery-about
-        .gallery-about__slide
-          .gallery-about__slide__img-block
-              img(src='img/slide-img-1.png' alt='')
-          .gallery-about__slide__title Christine
-          .gallery-about__slide__theme Fashion & Beauty блогер
-          .gallery-about__slide__text
-            p «Мой мир - мое вдохновение, я люблю созерцать все самое прекрасное
-              br
-              | и передавать свои эмоции окружающим. Я родилась в Латвии
-              br
-              | и мой город диктует стиль моей жизни»
+        swipe.my-swipe
+          swipe-item.slide1
+            .gallery-about__slide.gallery-about__slide__img-block
+                img(src='img/slide-img-1.png' alt='')
+            .gallery-about__slide__title Christine
+            .gallery-about__slide__theme Fashion & Beauty блогер
+            .gallery-about__slide__text
+              p «Мой мир - мое вдохновение, я люблю созерцать все самое прекрасное
+                br
+                |  и передавать свои эмоции окружающим. Я родилась в Латвии
+                br
+                |  и мой город диктует стиль моей жизни»
+          swipe-item.slide2
+            .gallery-about__slide.gallery-about__slide__img-block
+                img(src='img/slide-img-2.png' alt='')
+            .gallery-about__slide__title Polina
+            .gallery-about__slide__theme Fashion & lifestyle блогер
+            .gallery-about__slide__text
+              p «Фото - отражение моей жизни. Они разные, но в то же время гармонируют.
+                br
+                |  Я кофеман, авантюристка и путешественница.
+                br
+                |  Главные увлечения - фотография, съемка видео»
+          swipe-item.slide3
+            .gallery-about__slide.gallery-about__slide__img-block
+                img(src='img/slide-img-3.png' alt='')
+            .gallery-about__slide__title Alice
+            .gallery-about__slide__theme Fashion & lifestyle блогер
+            .gallery-about__slide__text
+              p «Хочу передать красоту повседневных вещей, на которые мы
+                br
+                |  не обращаем внимание. Поделиться с подписчиками эмоциями
+                br
+                |  о моем главном увлечении - музыка и игра на фортепиано»
+          swipe-item.slide4
+            .gallery-about__slide.gallery-about__slide__img-block
+                img(src='img/slide-img-4.png' alt='')
+            .gallery-about__slide__title Diana
+            .gallery-about__slide__theme Lifestyle блогер
+            .gallery-about__slide__text
+              p «Мой Инстаграмм это стильные, легкие фото, которые передают эмоции
+                br
+                |  и заставляют улыбаться! Люблю стрит-арт и одежду, которые удобно
+                br
+                |  носить и в которой я чувствую себя комфортно и уверенно!»
+
+
 
   .section.inform-item.about-mobile
     .section__content
+      .about-mobile__duplicate
+        .about-mobile__duplicate__title Шопинг твоих желаний
+        p Фильтруй по категориям,
+          br
+          | жми купить и консультант
+          br
+          | ответит в шопинг-чате.
       .inform-item__top-block.inform-item__bg-red
         .about-mobile__two-col
           .about-mobile__two-col__img
             .about-mobile__img-block
               img(src='img/iphone-1.png' alt='')
+              .video
+                video
+                  source(src="http://3wcode.com.ua/ostrog/video/main.mp4" type="video/mp4")
+                  source(src="http://3wcode.com.ua/ostrog/video/main.ogv" type="video/ogg")
+                  | Your browser does not support the video tag.
+                a(href='#' v-on:click="playVideo").play-btn
+                  i.ic-play-inverted
           .about-mobile__two-col__text
             .vertical-align-wrapper
               .about-mobile__two-col__title Шопинг твоих желаний
-              p Теперь жми Купить и консультант
+              p.mobile-hidden Фильтруй по категориям, жми купить
                 br
-                | ответит в шопинг-чате.
+                |  и консультант ответит в шопинг-чате.
               p Весь заказ, включая оплату и доставку,
                 br
-                | в одном окошке чата.
+                |  в одном окошке чата.
 
 
   .section.inform-item.about-mobile
     .section__content
+      .about-mobile__duplicate
+        .about-mobile__duplicate__title А ещё,
+          br
+          |  мы интегрировались
+          br
+          |  с Instagram!
+        p Теперь ты можешь
+          br
+          |  купить или сохранить
+          br
+          |  инстаграм-пост, оставив
+          br
+          |  комментарий в ленте.
       .inform-item__top-block.inform-item__bg-gradient
         .about-mobile__two-col
-          .about-mobile__two-col__text
+          .about-mobile__two-col__text.right
             .vertical-align-wrapper
               .about-mobile__two-col__title А ещё, мы интегрировались
                 br
-                | с Instagram!
-              p Теперь ты можешь купить или сохранить
+                |  с Instagram!
+              p.mobile-hidden Теперь ты можешь купить или сохранить
                 br
-                |то, что понравилось, просто
-                br
-                | оставив комментарий в ленте.
-              div
+                |  инстаграм-пост, оставив комментарий в ленте.
+              div.about-mobile__two-col__at
                 span @wantit
+                | * 
                 |  купить товар через наш шопинг-чат
-              div
+              div.about-mobile__two-col__at
                 span @savetrend
                 |  добавить в свою ленту трендов
+
+            span.note *только посты с тегом #trendever
           .about-mobile__two-col__img
             .about-mobile__img-block.about-mobile__img-block__right
               img(src='img/iphone-2.png' alt='')
-
+              .video
+                video
+                  source(src="http://3wcode.com.ua/ostrog/video/instagram.mp4" type="video/mp4")
+                  source(src="http://3wcode.com.ua/ostrog/video/instagram.ogv" type="video/ogg")
+                  | Your browser does not support the video tag.
+                a(href='#' v-on:click="playVideo").play-btn
+                  i.ic-play-inverted
   .section.about-us
     .section__content
       .about-us__logo
         a(href='#')
-          img(src='img/about-us-logo.svg' alt='')
+          img(src='img/about-us-logo.svg' alt='' itemprop="contentUrl")
       p Поможет найти и купить тренды
         br
-        | без наценки из первых рук!
+        |  без наценки из первых рук!
       a(href='#').btn-yellow.btn-yellow__m Перейти на сайт
-      p Делаем шопинг в Instagram удовольствием.
-        br
-        | И делаем это бесплатно ;)
+      p Превращаем Instagram-шопинг в удовольтсвие.
 
   .section.inform-item.footer
     .section__content
@@ -169,14 +237,16 @@
         .vertical-align-wrapper
           p У тебя свой бренд?
             br
-            | Хочешь продаваться у нас?
+            |  Хочешь продаваться у нас?
           a(href='#').btn-yellow.btn-yellow__m Узнай как
+  script
 
 </template>
 <script>
   import listen from 'event-listener';
   import { ratioFit } from 'utils';
   import HeaderComponent from 'base/header/header.vue';
+  import { Swipe, SwipeItem } from 'vue-swipe';
 
   export default {
     data(){
@@ -192,11 +262,6 @@
       }
     },
 
-    ready(){
-      this.updateMainVideoSize();
-
-      this.resizeEvent = listen(window, 'optimizedResize', this.updateMainVideoSize.bind( this ))
-    },
 
     methods: {
       updateMainVideoSize(){
@@ -206,10 +271,30 @@
         this.mainVideoWidth = sizes.width;
         this.mainVideoHeight = sizes.height;
       },
+      playVideo: function (e) {
+        var btn = e.currentTarget,
+            video = btn.previousSibling,
+            darknessBlock = video.parentNode;
+
+        btn.style.display = 'none';
+        video.play();
+        darknessBlock.classList.add('played');
+        video.addEventListener("ended", function(){
+          btn.style.display = 'block';
+          darknessBlock.classList.remove('played');
+        });
+        video.addEventListener("pause", function(){
+          btn.style.display = 'block';
+          darknessBlock.classList.remove('played');
+        });
+      }
     },
 
     components: {
-      HeaderComponent
+      HeaderComponent,
+        Swipe,
+        SwipeItem
     }
+
   }
 </script>
