3923ab98139e7ef8fc8cfced33e8a5dedbf68b90
dmgame <denis.m.pcspace@gmail.com>
2016-05-30 11:39:24 +0200

    add "more-btn" to main page

--- src/project/product-list/caption.vue
+++ src/project/product-list/caption.vue
@@ -4,6 +4,7 @@
   a.caption__play(v-link='{name: "why"}')
     i.ic-play
   .caption__description (смотрите видео)
+  a(href='./why').btn.fast__big__btn.__orange.btn_primary.btn__more Подробнее
   .caption__subtitle Ежедневные шопинг тренды
 </template>
 <script>
