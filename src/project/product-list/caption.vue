74633c98e813e404c3ecedfc37c506b11af4ab7c
Ruslan Yanberdin <happierall@gmail.com>
2016-06-10 01:38:52 +0300

    [REF] video on page info/user

--- src/project/product-list/caption.vue
+++ src/project/product-list/caption.vue
@@ -3,18 +3,18 @@
 
 .caption(v-if='!isAuth')
   span(id='how-it-work') Как это работает?
-  span(@click='videoShowed=true').caption__play
+  a(href="https://vimeo.com/167123446", target='_blank').caption__play
     i.ic-play
-  .caption__description (смотрите видео)
+  a.caption__description(href="https://vimeo.com/167123446", target='_blank') (смотрите видео)
   a(href='' v-link='{name: "info-user"}').btn.fast__big__btn.__orange.btn_primary.btn__more
     | Подробнее
   .caption__subtitle Ежедневные шопинг тренды
 
-.main-video(v-if='videoShowed')
-  i(@click='videoShowed=false').ic-close
-  iframe(src='https://player.vimeo.com/video/167123446?title=0&byline=0&portrait=0&autoplay=1',
-    frameborder='0', webkitallowfullscreen,
-    mozallowfullscreen, allowfullscreen)
+//- .main-video(v-if='videoShowed')
+//-   i(@click='videoShowed=false').ic-close
+//-   iframe(src='https://player.vimeo.com/video/167123446?title=0&byline=0&portrait=0&autoplay=1',
+//-     frameborder='0', webkitallowfullscreen,
+//-     mozallowfullscreen, allowfullscreen)
 </template>
 <script>
   import { isAuth } from 'vuex/getters/user.js';
