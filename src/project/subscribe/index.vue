d99ccd1b7397ba68265720286f70ab088396d789
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-08-15 11:46:04 +0300

    fix subscribe page

--- src/project/subscribe/index.vue
+++ src/project/subscribe/index.vue
@@ -5,7 +5,10 @@
    i.ic-close(@click="exitSubscribe")
   .subscribe-wrapper
    .subscribe-head Вы на тестовом сайте
-   .subscribe-note ...и бывают баги, о которых пишите пжл на hello@trendever.com &#128549 #[br]#[br]Хотите помочь сделать шопинг в инсте таким же удобным как в магазине? Тогда оставьте свой email – no spam &#128567
+   .subscribe-note ...и бывают баги, о которых пишите пжл на 
+   a(href="mailto:ello@trendever.com", target="_blank").subscribe-mail-link hello@trendever.com 
+   &#128549 
+   #[br]#[br]Хотите помочь сделать шопинг в инсте таким же удобным как в магазине? Тогда оставьте свой email – no spam &#128567
    .subscribe-input-wrapper
     i.subscribe-ic-mail
      img(src="icons/subscribe-mail.png")
