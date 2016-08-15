4530fd55e5ff75040a857576590e6d7009b29c35
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-08-15 12:31:04 +0300

    fix subscribe page

--- src/project/subscribe/index.vue
+++ src/project/subscribe/index.vue
@@ -6,7 +6,7 @@
   .subscribe-wrapper
    .subscribe-head Вы на тестовом сайте
    .subscribe-note ...и бывают баги, о которых пишите пжл на 
-   a(href="mailto:ello@trendever.com", target="_blank").subscribe-mail-link hello@trendever.com 
+   a(href="mailto:ello@trendever.com").subscribe-mail-link hello@trendever.com 
    &#128549 
    #[br]#[br]Хотите помочь сделать шопинг в инсте таким же удобным как в магазине? Тогда оставьте свой email – no spam &#128567
    .subscribe-input-wrapper
