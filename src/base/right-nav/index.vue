67c6d8689a9772630a3f7d7e8737f79984c41d22
riffca <riffca@yandex.ru>
2016-09-07 19:36:54 +0300

    add right nav counter

--- src/base/right-nav/index.vue
+++ src/base/right-nav/index.vue
@@ -4,16 +4,20 @@
     i.ic-user_menu
   .right-nav_i(:class='{"__active": current=="chat"}', v-link='{name: "chat_list"}')
     i.ic-chats_menu
+    .notify-cout(v-if="getGlobalNotifyCount")
+      span {{ getGlobalNotifyCount }}
   .right-nav_i(:class='{"__active": current=="feed"}', v-link='{name: "home"}')
     i.ic-gallery_menu
 </template>
 
 <script>
 import { isAuth } from 'vuex/getters/user';
+import { getGlobalNotifyCount } from 'vuex/getters/lead.js';
 export default {
   vuex:{
     getters:{
-      isAuth
+      isAuth,
+      getGlobalNotifyCount
     }
   },
   props:{
@@ -45,9 +49,10 @@ export default {
   position: absolute;
   top:0;
   right:0;
-  z-index: 9999;
+  z-index: 999;
 
   .right-nav_i {
+    position: relative;
     height: 50px;
     width: 50px;
     display: inline-block;
@@ -69,6 +74,26 @@ export default {
     i.ic-user_menu, i.ic-gallery_menu, i.ic-chats_menu  {
       font-size: 32px;
       color: white;
+    }
+
+    .notify-cout{
+      position: absolute;
+      z-index: 1000;
+      top: 3px;
+      right: 3px;
+      display: flex;
+      min-width: 23px;
+      height: 23px;
+      padding: 0 3px;
+
+      border-radius: 15px;
+      background: #EF3758;
+      font-size: 10px;
+      align-items: center;
+      justify-content: center;
+      span {
+        color: white;
+      }
     }
   }
 }
