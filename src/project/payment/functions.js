b5a09ffe5d29d996ce3ee3e3e3dbf7a02db91d15
lopan <anlopan@gmail.com>
2016-09-22 11:34:12 +0500

    summ final

--- src/project/payment/functions.js
+++ src/project/payment/functions.js
@@ -1,13 +1,15 @@
 export const getPaymentAmmount = (msg) => {
   let _currency = JSON.parse(msg.parts[0].content).currency
   let amount = JSON.parse(msg.parts[0].content).amount;
+  if (_currency === 2){
+    amount = amount / 100;
+  }
+  
   if (amount * 1.015 - amount < 50){
   	amount += 50;	
   }else{
   	amount = amount * 1.015;
   }
-  if (_currency === 2){
-  	amount = amount / 100;
-  }
+  
   return Math.round(amount);
 }
