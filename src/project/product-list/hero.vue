638e483e0866d790849d1980c24219b575278b73
lopan <anlopan@gmail.com>
2016-10-04 14:21:35 +0500

    hero page br condition in title

--- src/project/product-list/hero.vue
+++ src/project/product-list/hero.vue
@@ -75,7 +75,7 @@
        .profile-header__mobile-slider-slide
         slider
      .hero__content__logo__mobile
-    .hero__content__description Шопинг в Instagram #[br] стал проще!
+    .hero__content__description Шопинг в Instagram #[br(v-if="isMobile")] стал проще!
     button.btn.btn_primary.__orange.__xl.enter__btn.fast__big__btn( v-link="{ name: 'signup' }" v-if="isStandalone") ВХОД И РЕГИСТРАЦИЯ
     button.btn.btn_primary.__orange.__xl.enter__btn.fast__big__btn( v-link="{ name: 'app' }" v-else) СКАЧАТЬ ПРИЛОЖЕНИЕ
     .hero__content__footer
