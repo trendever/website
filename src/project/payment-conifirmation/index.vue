ab72b0cd31365875bf4892a043d8b33d2bbe64d6
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-10-12 18:22:08 +0300

    add fonts on payment-conifirmation page

--- src/project/payment-conifirmation/index.vue
+++ src/project/payment-conifirmation/index.vue
@@ -18,12 +18,14 @@ export default {
 </script>
 
 <style lang="css">
+@import 'base/vars/vars.pcss';
   .wrapper{
     height: 100%;
     background-color: #0E3333;
     color: #fff;
     font-size: 36px;
     text-align: center;
+    font-family: $font__family__regular;
   }
   .header{
     font-size: 54px;
