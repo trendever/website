f5afb8ecc6088b94211c2e3c9a84c8ca193a8c8a
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-10-14 01:25:33 +0300

    add monetization page

--- src/project/monetization/index.vue
+++ src/project/monetization/index.vue
@@ -3,5 +3,25 @@
  .monetization
   i.ic-close(@click='closePage')
   .monetization__days-to-end 7
-  .monetization__text дней осталось до окончания бесплатного периода
+  .monetization__text дней осталось до окончания #[br] бесплатного периода
+  .monetization__text.bot Выбери, как тебе выгоднее #[br] оплачивать услуги Trendever
+  .monetization__btn
+   button 1990 В МЕСЯЦ
+  .monetization__i.ic-monetization-icon
+  .monetization__btn
+   button 5.5% ЗА СДЕЛКУ
+  .monetization__accept-btn
+   button ПОКА НЕ УВЕРЕН
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {}
+}
+</script>
