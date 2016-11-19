5e3c9969af59c940c5cf77c828da586076c8060d
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-11-19 02:47:10 +0200

    fixes on profile page (trendever desk&mob)

--- src/project/profile/index.vue
+++ src/project/profile/index.vue
@@ -54,6 +54,7 @@ scroll-component(v-if="isDone", class="profile-cnt", v-on:click="outerCloseMenu"
 
           .profile_info_img()
             img(:src="getUserPhoto")
+          .empty-img(v-if="noLikes && noProducts && isSelfPage")
           .profile_info_about(v-if="false")
             .profile_info_about_type Магазин
               #[br]
@@ -70,9 +71,9 @@ scroll-component(v-if="isDone", class="profile-cnt", v-on:click="outerCloseMenu"
             .profile_info_count_t.following Подписки
             .profile_info_count 53
 
-        .profile_desc
+        .profile_desc.less(v-on:click="this.isMoreClass = !this.isMoreClass" v-bind:class="{ more : isMoreClass, less: !isMoreClass}")
           .profile_desc_t(v-if="getSlogan") {{getSlogan}}
-          .profile_desc_caption.less(v-if="getUserCaption", v-on:click="this.isMoreClass = !this.isMoreClass" v-bind:class="{ more : isMoreClass, less: !isMoreClass}") {{{getUserCaption | captionSpaces}}}
+          .profile_desc_caption(v-if="getUserCaption") {{{getUserCaption | captionSpaces}}}
 
         .profile_insta-link(v-if="$route.name === 'profile' && shopId !== 1 && isMobile")
           .insta-link-text ссылка на эту витрину
@@ -91,12 +92,15 @@ scroll-component(v-if="isDone", class="profile-cnt", v-on:click="outerCloseMenu"
          span #[br] потому что ты пока ничего #[br] не сохранил
         .profile_no-goods-guest(v-if="noLikes && noProducts && $route.name === 'user'") Пока здесь пусто ;( #[br] Пользователь еще не добавил #[br] тренды в свою ленту
         .profile_no-goods-banner(v-if="noLikes && noProducts && isSelfPage") Нажми Сохранить&nbsp
-         span под товаром #[br.break_1] или&nbsp
+         span под товаром #[br] или&nbsp
          | напиши&nbsp
-         span.save @savetrend&nbsp #[br(v-if="!isMobile")]
+         span.save @savetrend&nbsp
          span под постом в #[br.break_2] Instagram, #[br(v-if="!isMobile")] чтобы добавить тренд сюда в ленту
 
         button.btn.btn_primary.__orange.__xl.fast__big__btn.btn_fixed-bottom.profile-btn(@click="subscrib//e") ПОДПИСАТЬСЯ
+       template(v-if="loaded")
+         .find-bloger-btn(v-if='isSelfPage && showBloger && isSupplier', @click="buyServiceProduct") Найти блогера
+         .how-to-sell-btn( v-link="{name: 'info-instructions-1'}", v-if="isMobile && noLikes && noProducts") Как начать продавать?
 
       //- лайки
       photos-component(
@@ -117,9 +121,7 @@ scroll-component(v-if="isDone", class="profile-cnt", v-on:click="outerCloseMenu"
     .main-text {{message}}
     .button-text(v-on:click="showPopup = false")
       span OK
-  template(v-if="loaded")
-    .find-bloger-btn(v-if='isSelfPage && isMobile && showBloger', @click="buyServiceProduct") Найти блогера
-    .how-to-sell-btn( v-link="{name: 'info-instructions-1'}", v-if="isMobile && noLikes && noProducts") Как начать продавать?
+
 .help-wrapper(v-if='isFirst')
   .attention(v-if='isFirst')
     p Для корректного отображения подсказок переверните устройство в портретную ориентацию
