24d2281e4680f8d0df6a3045cd092c83ed8cbfea
riffca <riffca@yandex.ru>
2016-12-03 14:36:42 +0300

    fix username check

--- src/project/auth/confirm-sms.vue
+++ src/project/auth/confirm-sms.vue
@@ -171,15 +171,9 @@ div
         this.isCompleted = true;
         this.$els.confirmBtn.focus();
 
-        // if(user.name !== this.authData.username && this.authData.instagram) {
-        //   if(user.name) {
-        //       this.anotherName = user.name;
-        //   }
-        // }
-
         if(user.instagram_username) {
           if(user.instagram_username !== this.authData.username){
-            this.anotherName = user.name;
+            this.anotherName = user.instagram_username;
           }
         }
 
